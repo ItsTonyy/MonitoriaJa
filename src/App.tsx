fdfe44803b35f4f665cffbf76ba6c678de791c7e
Paulo Eduardo <pauloeduardorj21@outlook.com>
2025-09-20 17:32:51 -0300

    feat: tela de cadastro de monitores implementada em react

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css';
+import CadastroMonitor from './pages/CadastroMonitor';
 
 function App() {
   return (
+    /*
     <div className="App">
       <header className="App-header">
         <h1>Monitoria Já</h1>
         <p>Conectando monitores de todo o Brasil a qualquer aluno de forma remota.</p>
         <p>Projeto desenvolvido por: Tony, Guilherme Andrade, Otávio, Rafael Penela, Paulo</p>
       </header>
+    </div>
+    */
+    <div className="App">
+      <CadastroMonitor />
     </div>
   );
 }
