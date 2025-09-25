0c2e1ac9b87677ea87b4b632c7a0c71e81ab979d
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-09-25 01:38:47 -0300

    feat(header): novo header adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/Login/LoginPage";
-import RecuperarSenhaPage from "./pages/Login/recuperarSenha/RecuperarSenhaPage";
-import AvaliacaoPosAulaPage from "./pages/Login/AvaliacaoPosAula/AvaliacaoPosAulaPage";
-import ComentariosAvaliacaoPage from "./pages/Login/ComentariosAvaliacao/ComentariosAvaliacaoPage";
-import ListaMonitores from "./components/ListaMonitores";
-import ListaAgendamentos from "./components/ListaAgendamentos";
-import CadastroMonitor from "./pages/CadastroMonitor";
-import ResponsiveAppBar from "./components/login-form/AppBar";
-import Footer from "./components/footer";
-import DetalhesMonitor from "./components/detalhesMonitor/DetalhesMonitor";
-import AgendamentoMonitor from "./components/detalhesMonitor/agendamentoMonitor";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/Login/LoginPage';
+import RecuperarSenhaPage from './pages/Login/recuperarSenha/RecuperarSenhaPage';
+import AvaliacaoPosAulaPage from './pages/Login/AvaliacaoPosAula/AvaliacaoPosAulaPage';
+import ComentariosAvaliacaoPage from './pages/Login/ComentariosAvaliacao/ComentariosAvaliacaoPage';
+import ListaMonitores from './components/ListaMonitores';
+import ListaAgendamentos from './components/ListaAgendamentos';
+import CadastroMonitor from './pages/CadastroMonitor';
+import ResponsiveAppBar from './components/login-form/AppBar';
+import Footer from './components/footer';
+import DetalhesMonitor from './components/detalhesMonitor/DetalhesMonitor';
+import AgendamentoMonitor from './components/detalhesMonitor/agendamentoMonitor';
+import AppNavbar from './components/appNavBar';
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <ResponsiveAppBar />
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/recuperar-senha" element={<RecuperarSenhaPage />} />
-            <Route path="/avaliacao-pos-aula" element={<AvaliacaoPosAulaPage />} />
-            <Route path="/comentarios-avaliacao" element={<ComentariosAvaliacaoPage />} />
-            <Route path="/lista-monitores" element={<ListaMonitores />} />
-            <Route path="/lista-agendamentos" element={<ListaAgendamentos />} />
-            <Route path="/cadastro-monitor" element={<CadastroMonitor />} />
-            <Route path="/detalhes-monitor" element={<DetalhesMonitor />} />
-            <Route path="/agendamento-monitor" element={<AgendamentoMonitor />} />
-          </Routes>
-        <Footer />
-      </BrowserRouter>
+    <BrowserRouter>
+      <AppNavbar />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/recuperar-senha" element={<RecuperarSenhaPage />} />
+        <Route path="/avaliacao-pos-aula" element={<AvaliacaoPosAulaPage />} />
+        <Route path="/comentarios-avaliacao" element={<ComentariosAvaliacaoPage />} />
+        <Route path="/lista-monitores" element={<ListaMonitores />} />
+        <Route path="/lista-agendamentos" element={<ListaAgendamentos />} />
+        <Route path="/cadastro-monitor" element={<CadastroMonitor />} />
+        <Route path="/detalhes-monitor" element={<DetalhesMonitor />} />
+        <Route path="/agendamento-monitor" element={<AgendamentoMonitor />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
