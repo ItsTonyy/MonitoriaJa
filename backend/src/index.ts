3487e4fee353346b66d15b82760c8b07c835e9d5
RafaelPenela <rafaelpenela30@gmail.com>
2025-11-24 15:49:17 -0300

    perfil: lidando com fotos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,10 +9,42 @@ import disponibilidadeRoutes from "./routes/disponibilidadeRoutes";
 import avaliacaoRoutes from "./routes/avaliacaoRoutes";
 import notificacaoRoutes from "./routes/notificacaoRoutes";
 import loginRoutes from "./routes/login";
+import uploadRoutes from "./routes/uploadRoutes";
 
 // Se usar dotenv para variáveis de ambiente:
 import dotenv from "dotenv";
 dotenv.config();
+
+// configuração do swaggerUI & swaggerJsDoc pra documentação
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerJSDoc = require("swagger-jsdoc");
+const path = require("path");
+
+const swaggerDefinition = {
+  openapi: "3.1.0",
+  info: {
+    title: "Express API for the MonitoriaJa project.",
+    version: "1.0.0",
+  },
+  servers: [
+    {
+      url: "http://localhost:3001",
+      description: "Servidor de desenvolvimento",
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: [
+    path.join(__dirname, "routes", "*.ts"),
+    path.join(__dirname, "routes", "*.js"),
+  ],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
 
 const app: Application = express();
 
@@ -26,7 +58,10 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use(express.static("public"));
+
 // rotas aqui...
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cors());
 app.use("/disciplina", disciplinaRoutes);
 app.use("/agendamento", agendamentoRoutes);
@@ -35,6 +70,7 @@ app.use("/usuario", usuarioRoutes);
 app.use("/disponibilidade", disponibilidadeRoutes);
 app.use("/avaliacao", avaliacaoRoutes);
 app.use("/notificacao", notificacaoRoutes);
+app.use("/upload", uploadRoutes);
 app.use(loginRoutes)
 // Conexão com o banco de dados e inicialização do servidor
 
